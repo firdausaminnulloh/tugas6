ace095b2926cf47a54b700e7e4043bb9c2277121
firdaus aminnulloh <firdaus_23510027@stimata.ac.id>
2023-11-16 21:04:46 +0700

    Menggabungkan string nama depan dan belakang

---
+++ index.php
@@ -0,0 +1,7 @@
+<!-- index.php -->
+<?php
+    $nama_depan = "firdaus";
+    $nama_belakang = "aminnulloh";
+    $nama_lengkap = $nama_depan . " " . $nama_belakang;
+    echo "Nama lengkap: " . $nama_lengkap;
+?>
